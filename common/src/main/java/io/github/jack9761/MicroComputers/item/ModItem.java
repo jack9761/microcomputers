20b93d59f37703511bb328f5403b742dffae7d27
jack9761 <jackmietz@gmail.com>
2025-06-16 14:18:08 -0700

    Instruction manual texture and basic item added, gitignore updated, renamed from Mexican Spanish to European Spanish so fallback works

--- common/src/main/java/io/github/jack9761/MicroComputers/item/ModItem.java
+++ common/src/main/java/io/github/jack9761/MicroComputers/item/ModItem.java
@@ -10,5 +10,6 @@ import net.minecraft.world.item.Item;
 
 public class ModItem {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MicroComputers.MOD_ID, Registries.ITEM);
-    public static final RegistrySupplier<Item> SILICON =  ITEMS.register("silicon",() -> new Item(new Item.Properties().arch$tab(ModCreativeTab.MICROCOMPUTERS_CREATIVE_TAB)));
+    public static final RegistrySupplier<Item> SILICON = ITEMS.register("silicon",() -> new Item(new Item.Properties().arch$tab(ModCreativeTab.MICROCOMPUTERS_CREATIVE_TAB)));
+    public static final RegistrySupplier<Item> MANUAL = ITEMS.register("instruction_manual", ()-> new Item(new Item.Properties().arch$tab(ModCreativeTab.MICROCOMPUTERS_CREATIVE_TAB)));
 }
